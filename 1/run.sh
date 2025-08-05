86e3fd5b1d2d8e662d9fb77cb5e7470951deabce
AliAbbas-1 <alialiabbas110110@gmail.com>
2025-08-05 12:12:27 +0500

    Renamed venv to .venv

--- 1/run.sh
+++ 1/run.sh
@@ -11,12 +11,12 @@ setup_and_run() {
 
     cd "$dir"
 
-    if [ ! -d "venv" ]; then
+    if [ ! -d ".venv" ]; then
         echo "Creating virtual environment in $dir..."
-        python3 -m venv venv
+        python3 -m .venv .venv
     fi
 
-    source venv/bin/activate
+    source .venv/bin/activate
     echo "Installing dependencies for $dir..."
     pip install --upgrade pip > /dev/null
     pip install -r requirements.txt
