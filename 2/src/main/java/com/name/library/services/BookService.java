3284e41bdeed324b16a3a8d2f35b418345c3fed1
AliAbbas-1 <alialiabbas110110@gmail.com>
2025-08-06 13:44:45 +0500

    service and dtos

--- 2/src/main/java/com/name/library/services/BookService.java
+++ 2/src/main/java/com/name/library/services/BookService.java
@@ -1,5 +1,23 @@
 package com.name.library.services;
 
-public class BookService {
+import java.util.List;
+import java.util.Optional;
 
+import com.name.library.dto.BookDto;
+
+public interface BookService {
+
+    List<BookDto> getAllBooks();
+    
+    Optional<BookDto> getBookById(String id);
+
+    BookDto addBook(BookDto bookDto);
+
+    Optional<BookDto> updateBook(String id, BookDto bookDto);
+
+    void deleteBook(String id);
+    
+    void markBookAsAvailable(String id);
+    
+    void markBookAsUnavailable(String id);
 }
